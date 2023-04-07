a7d62837303cf92a8fcf5f374fc12a91e0e7f7f2
KimYongJae <yjkim1@jastech.co.kr>
2023-04-07 16:28:24 +0900

    Test 프로젝트 재 생성 후 업로드

---
+++ Source/Test/MainForm.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Test
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pnlTest = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.pnlThumbnail = new System.Windows.Forms.Panel();
+            this.pnlDisplay = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 88F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 525F));
+            this.tableLayoutPanel1.Controls.Add(this.pnlTest, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1161, 554);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // pnlTest
+            // 
+            this.pnlTest.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlTest.Location = new System.Drawing.Point(639, 2);
+            this.pnlTest.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlTest.Name = "pnlTest";
+            this.pnlTest.Size = new System.Drawing.Size(519, 550);
+            this.pnlTest.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.btnTest);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(551, 2);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(82, 550);
+            this.panel2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 206);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 66);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 116);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 66);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "add roi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(3, 38);
+            this.btnTest.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(88, 66);
+            this.btnTest.TabIndex = 0;
+            this.btnTest.Text = "button1";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.pnlThumbnail, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.pnlDisplay, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(542, 548);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // pnlThumbnail
+            // 
+            this.pnlThumbnail.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlThumbnail.Location = new System.Drawing.Point(3, 450);
+            this.pnlThumbnail.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlThumbnail.Name = "pnlThumbnail";
+            this.pnlThumbnail.Size = new System.Drawing.Size(536, 96);
+            this.pnlThumbnail.TabIndex = 3;
+            // 
+            // pnlDisplay
+            // 
+            this.pnlDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlDisplay.Location = new System.Drawing.Point(3, 2);
+            this.pnlDisplay.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlDisplay.Name = "pnlDisplay";
+            this.pnlDisplay.Size = new System.Drawing.Size(536, 444);
+            this.pnlDisplay.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(-6, 332);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 66);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1161, 554);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel pnlDisplay;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel pnlTest;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Panel pnlThumbnail;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Source/Test/MainForm.cs
@@ -0,0 +1,127 @@
+﻿using Cognex.VisionPro;
+using Cognex.VisionPro.PMAlign;
+using Jastech.Apps.Winform.Settings;
+using Jastech.Framework.Imaging.Helper;
+using Jastech.Framework.Imaging.VisionPro;
+using Jastech.Framework.Imaging.VisionPro.VisionAlgorithms;
+using Jastech.Framework.Util.Helper;
+using Jastech.Framework.Winform.Forms;
+using Jastech.Framework.Winform.VisionPro.Controls;
+using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class MainForm : Form
+    {
+        CogDisplayControl display;
+        CogPMAlignTool CogPMAlignTool;
+        CogThumbnailControl thumbnailDisplay;
+        ICogImage CogImage;
+        ModelForm form;
+        CogPatternMatchingParamControl CogPatternMatchingParamControl = new CogPatternMatchingParamControl();
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            LogHelper.Initialize(@"D:\0.Works\1.Programs\ATT\Jastech.Apps\Runtime\Log");
+            LogHelper.Error(ErrorType.Grabber, "Test");
+            display = new CogDisplayControl();
+            display.Dock = DockStyle.Fill;
+         
+            pnlDisplay.Controls.Add(display);
+
+
+            //CogPatternMatchingParamControl.Dock = DockStyle.Fill;
+            thumbnailDisplay = new CogThumbnailControl();
+            thumbnailDisplay.Dock = DockStyle.Fill;
+            pnlThumbnail.Controls.Add(thumbnailDisplay);
+
+            display.DrawViewRectEventHandler += thumbnailDisplay.DrawViewRect;
+            //Settings.Instance().Initialize();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            //string fileName = @"D:\back\06_35_19_0_0\06_35_19_0_0.bmp";
+            string fileName = @"D:\Image.png";
+            CogImage = CogImageHelper.Load(fileName);
+            display.SetImage(CogImage);
+            thumbnailDisplay.SetThumbnailImage(CogImage);
+        }
+        CogPatternMatching cogPMAlign = new CogPatternMatching();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //display.
+            //display.ClearGraphic();
+       
+            //ICogImage cogImage = display.GetImage();
+
+            //double centerX = cogImage.Width / 2;
+            //double centerY = cogImage.Height / 2;
+
+            //CogRectangle rect2 = CogImageHelper.CreateRectangle(centerX - display.GetPan().X, centerY - display.GetPan().Y, 100, 100);
+            //CogRectangle rect = CogImageHelper.CreateRectangle(centerX , centerY, cogImage.Width, cogImage.Height);
+
+            //cogPMAlign.SetTrainRegion(rect2);
+
+            //display.AddGraphics("tool", cogPMAlign.GetTrainRegion());
+
+            //cogPMAlign.SetSearchRegion(rect);
+
+            //display.AddGraphics("tool", cogPMAlign.GetSearchRegion());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            //thumbnailDisplay.Test();
+
+            //string filePath = @"D:\06_35_19_0_0.bmp";
+
+            //ICogImage image = CogImageHelper.Load(filePath);
+
+            //display.SetImage(image);
+            //thumbnailDisplay.SetThumbnailImage(image);
+                
+            //CogImageHelper.Save(g, @"d:\1234.bmp");
+            //ModelForm form = new ModelForm();
+            //form.ModelPath = @"D:\1.Programs\Jastech.Apps\Jastech.Apps\Runtime\Model";
+            //form.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Stopwatch sw = new Stopwatch();
+            Mat image1 = new Mat(@"D:\0.Works\3.테스트이미지\06_35_19_0_0.bmp");
+
+            //sw.Restart();
+            //Mat mat1 = Transpose(image1);
+            //sw.Stop();
+            //Debug.WriteLine(sw.ElapsedMilliseconds.ToString());
+
+            //mat1.SaveImage(@"D:\trans.bmp");
+
+            //sw.Reset();
+            //sw.Restart();
+            //Mat mat2 = CvHelper.Rotate(image1, -90);
+            //sw.Stop();
+            //Debug.WriteLine(sw.ElapsedMilliseconds.ToString());
+
+            //mat1.SaveImage(@"D:\rotate.bmp");
+
+        }
+    }
+}
