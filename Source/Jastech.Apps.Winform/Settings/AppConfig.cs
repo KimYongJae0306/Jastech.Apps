f43fcbd5fe64ab92ae57daad4638a3cc69b62304
KimYongJae <yjkim1@jastech.co.kr>
2023-04-13 17:14:43 +0900

    통신 모듈 변경으로 인한 수정

--- Source/Jastech.Apps.Winform/Settings/AppConfig.cs
+++ Source/Jastech.Apps.Winform/Settings/AppConfig.cs
@@ -1,9 +1,12 @@
 ﻿using Jastech.Apps.Structure;
 using Jastech.Apps.Structure.Data;
 using Jastech.Framework.Comm;
+using Jastech.Framework.Comm.Protocol;
 using Jastech.Framework.Config;
 using Jastech.Framework.Device.Cameras;
 using Jastech.Framework.Device.LightCtrls;
+using Jastech.Framework.Device.LightCtrls.Lvs;
+using Jastech.Framework.Device.LightCtrls.Lvs.Parser;
 using Jastech.Framework.Device.Motions;
 using Jastech.Framework.Imaging;
 using Newtonsoft.Json;
@@ -80,30 +83,26 @@ namespace Jastech.Apps.Winform.Settings
             else
             {
                 var camera0 = new CameraVieworksVT(CameraName.LinscanVT0.ToString(), 4096, 1024, ColorFormat.Gray, SensorType.Line);
+                camera0.SerialPortComm = new SerialPortComm
+                {
+                    PortName = "COM6",
+                    BaudRate = 115200,
+                };
                 config.Add(camera0);
 
                 var camera1 = new CameraMil(CameraName.LinscanMIL0.ToString(), 4096, 1024, ColorFormat.Gray, SensorType.Line);
                 config.Add(camera1);
 
-                var motion = new ACSMotion("Motion", 3);
+                var motion = new ACSMotion("Motion", 3, ACSConnectType.Ethernet);
+                motion.IpAddress = "10.0.0.100";
                 config.Add(motion);
 
-                var light1 = new LvsLightCtrl("LvsLight12V", 6);
-                light1.SerialPortProp = new SerialPortInfo
-                {
-                    BaudRate = 9600,
-                    PortName = "COM2",
-                };
+                var light1 = new LvsLightCtrl("LvsLight12V", 6, new SerialPortComm("COM2", 9600), new LvsSerialParser());
                 light1.ChannelNameMap["Ch.Blue"] = 0;
                 light1.ChannelNameMap["Ch.RedSpot"] = 1;
                 config.Add(light1);
 
-                var light2 = new LvsLightCtrl("LvsLight24V", 6);
-                light2.SerialPortProp = new SerialPortInfo
-                {
-                    BaudRate = 9600,
-                    PortName = "COM3",
-                };
+                var light2 = new LvsLightCtrl("LvsLight24V", 6, new SerialPortComm("COM3", 9600), new LvsSerialParser());
                 light2.ChannelNameMap["Ch.RedRing"] = 0;
                 config.Add(light2);
             }
