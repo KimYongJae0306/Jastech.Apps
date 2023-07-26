2ffaff147861dc0ce1fd5f657c7c2e31e5f0a09a
Young Jin Kim <oz1995@jastech.co.kr>
2023-07-26 15:07:19 +0900

    Program.cs  1) PLC 시작 주소 등 PC 별로 달라지는 설정이 있어 개별 초기화 필요  2) 프로그램 시작 시 타입에 따라 MachineConfig이 다르게 초기화함 + ProgramSelectForm.cs

--- Source/Jastech.Apps.Winform/UI/Forms/ProgramSelectForm.cs
+++ Source/Jastech.Apps.Winform/UI/Forms/ProgramSelectForm.cs
@@ -1,20 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace Jastech.Apps.Winform.UI.Forms
 {
     public partial class ProgramSelectForm : Form
     {
+        public string SelectedProgramType => cbxProgramTypes.Text;
+
         public ProgramSelectForm()
         {
             InitializeComponent();
+        }
+
+        public void SetList(string[] types)
+        {
+            cbxProgramTypes.Items.Clear();
+            cbxProgramTypes.Items.AddRange(types);
+            cbxProgramTypes.SelectedIndex = 0;
         }
     }
 }
