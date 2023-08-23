f200b763c16fbafe7bec3626f5ff4735033a73d1
Park Junghwan <parkjh9010@gmail.com>
2023-08-23 21:48:22 +0900

    tlqkf skew

--- Source/Jastech.Apps.Winform/UI/Controls/AlignResultDisplayControl.cs
+++ Source/Jastech.Apps.Winform/UI/Controls/AlignResultDisplayControl.cs
@@ -3,6 +3,7 @@ using Jastech.Apps.Structure;
 using Jastech.Apps.Structure.Data;
 using Jastech.Apps.Winform;
 using Jastech.Apps.Winform.UI.Controls;
+using Jastech.Framework.Imaging.Helper;
 using Jastech.Framework.Imaging.Result;
 using Jastech.Framework.Util.Helper;
 using Jastech.Framework.Winform.VisionPro.Controls;
@@ -177,40 +178,29 @@ namespace ATT_UT_IPAD.UI.Controls
                     // 정환
                     foreach (var result in tabInspResult.AlignResult.LeftX.AlignResultList)
                     {
-                        //PointF orginPoint = new PointF(tabInspResult.CogImage.Width / 2, tabInspResult.CogImage.Height / 2);
-                        //PointF fpcCenter = new PointF((float)(result.FpcCenterX - orginPoint.X), (float)(result.FpcCenterY - orginPoint.Y));
-                        //PointF panelCenter = new PointF((float)(result.PanelCenterX - orginPoint.X), (float)(result.PanelCenterY - orginPoint.Y));
+                        var rad = MathHelper.DegToRad(90);
 
-                        //double m = MathHelper.RadToDeg(result.FpcSkew);
+                        PointF fpcCenter = new PointF((float)(result.FpcCenterX), (float)(result.FpcCenterY));
+                        var fpcSkew = result.FpcSkew;
 
-                        //var degree = Math.Atan(m) * 180.0 / Math.PI;
-                        //degree += 320;
-                        //var g22222 = Math.Tan(degree * Math.PI / 180);
-                        ////y = mx + b
+                        CogLineSegment fpcLine = new CogLineSegment();
+                        fpcLine.Color = CogColorConstants.Blue;
+                        fpcLine.SetStartLengthRotation(fpcCenter.X, fpcCenter.Y, 450, rad + fpcSkew);
+                        InspAlignDisplay.DrawLine(fpcLine);
 
-                        //var b = fpcCenter.Y - (g22222 * fpcCenter.X);
 
-                        //var x = (panelCenter.Y - b ) / g22222;
+                        PointF panelCenter = new PointF((float)(result.PanelCenterX), (float)(result.PanelCenterY));
+                        var panelSkew = result.PanelSkew;
 
-                        //CogLineSegment fpcLine = new CogLineSegment();
-                        ////fpcLine.SetStartEnd(fpcCenter.X + orginPoint.X, fpcCenter.Y +orginPoint.Y, x + orginPoint.X, panelCenter.Y + orginPoint.Y);
-                        //fpcLine.SetStartLengthRotation(fpcCenter.X + orginPoint.X, fpcCenter.Y + orginPoint.Y, 500, g22222);
-                        ////fpcLine.SetStartLengthRotation(fpcCenter.X, fpcCenter.Y, x, m);
-
-                        ////fpcLine.SetStartLengthRotation(fpcCenter.X, fpcCenter.Y, x, fpcDegree);
-                        //fpcLine.Color = CogColorConstants.Blue;
-                        //InspAlignDisplay.DrawLine(fpcLine);
-
-                        //CogLine cogLine = new CogLine();
-                        //cogLine.set(fpcCenter.X, fpcCenter.Y, result.FpcSkew, result.PanelSkew);
-                        //InspAlignDisplay.DrawLine(cogLine);
-
+                        CogLineSegment panelLine = new CogLineSegment();
+                        panelLine.Color = CogColorConstants.Red;
+                        panelLine.SetStartLengthRotation(panelCenter.X, panelCenter.Y, 450, rad + panelSkew);
+                        InspAlignDisplay.DrawLine(panelLine);
                     }
                 }
             }
             else
                 InspAlignDisplay.ClearImage();
-          
         }
 
         private void UpdateImage(int tabNo)
