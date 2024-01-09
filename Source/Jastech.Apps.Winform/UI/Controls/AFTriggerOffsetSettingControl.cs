63872ef172f7b9c241edfc93a271fc79cbf9b1d1
Kang Sung Hyun <hobak728@gmail.com>
2024-01-09 17:31:20 +0900

    add laf trigger offset limit 설정

--- Source/Jastech.Apps.Winform/UI/Controls/AFTriggerOffsetSettingControl.cs
+++ Source/Jastech.Apps.Winform/UI/Controls/AFTriggerOffsetSettingControl.cs
@@ -9,6 +9,10 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Jastech.Apps.Structure.Data;
 using Jastech.Framework.Winform.Helper;
+using Jastech.Apps.Structure;
+using Jastech.Apps.Winform.Service.Plc;
+using Jastech.Framework.Winform.Forms;
+using Jastech.Framework.Structure;
 
 namespace Jastech.Apps.Winform.UI.Controls
 {
@@ -59,16 +63,21 @@ namespace Jastech.Apps.Winform.UI.Controls
                 return CurrentTab.LafTriggerOffset;
         }
 
-
         private void lblLeftOffset_Click(object sender, EventArgs e)
         {
             if (CurrentTab == null)
                 return;
-
+            
+            string oldOffset = lblLeftOffset.Text;
             float leftOffset = KeyPadHelper.SetLabelFloatData((Label)sender);
 
-            GetTriggerOffset().Left = leftOffset;
-            lblLeftOffset.Text = leftOffset.ToString();
+            if (CheckOffsetRange(leftOffset, MarkDirection.Left))
+            {
+                GetTriggerOffset().Left = leftOffset;
+                lblLeftOffset.Text = leftOffset.ToString();
+            }
+            else
+                lblLeftOffset.Text = oldOffset;
         }
 
         private void lblRightOffset_Click(object sender, EventArgs e)
@@ -76,11 +85,53 @@ namespace Jastech.Apps.Winform.UI.Controls
             if (CurrentTab == null)
                 return;
 
+            string oldOffset = lblRightOffset.Text;
             float rightOffset = KeyPadHelper.SetLabelFloatData((Label)sender);
 
-            GetTriggerOffset().Right = rightOffset;
-            lblRightOffset.Text = rightOffset.ToString();
+            if (CheckOffsetRange(rightOffset, MarkDirection.Right))
+            {
+                GetTriggerOffset().Right = rightOffset;
+                lblRightOffset.Text = rightOffset.ToString();
+            }
+            else
+                lblRightOffset.Text = oldOffset;
         }
+
         #endregion
+        private bool CheckOffsetRange(float offset, MarkDirection direction)
+        {
+            AppsInspModel inspModel = ModelManager.Instance().CurrentModel as AppsInspModel;
+            var materialInfo = inspModel.MaterialInfo;
+            var tabNo = CurrentTab.Index;
+
+            double distance = 0.0;
+
+            if (MarkDirection.Left == direction)
+            {
+                if (tabNo == 0)
+                    distance = materialInfo.PanelEdgeToFirst_mm;
+                else
+                    distance = materialInfo.GetTabToTabDistance(tabNo - 1, inspModel.TabCount) / 2;
+            }
+
+            if (MarkDirection.Right == direction)
+            {
+                if (tabNo == inspModel.TabCount - 1)    //TabCount 맞추기위해 -1
+                    distance = materialInfo.PanelEdgeToFirst_mm;
+                else
+                    distance = materialInfo.GetTabToTabDistance(tabNo, inspModel.TabCount) / 2;
+            }
+
+            if (Math.Abs(offset) >= distance)
+            {
+                string errorMessage = string.Format($"Enter a value less than ±{distance}");
+                MessageConfirmForm form = new MessageConfirmForm();
+                form.Message = errorMessage;
+                form.ShowDialog();
+                return false;
+            }
+
+            return true;
+        }
     }
 }
